23fc25fc0f59f85ac2809db4b762e754a0ff2eda
JoelEspinal <joel_espinal@claro.com.do>
2024-03-11 15:22:02 -0400

    Creat project

---
+++ Buisness Card/Buisness Card/Buisness_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Buisness_CardApp.swift
+//  Buisness Card
+//
+//  Created by Joel Espinal (ClaroDom) on 11/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Buisness_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Buisness Card/Buisness Card/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Buisness Card
+//
+//  Created by Joel Espinal (ClaroDom) on 11/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
